4a994909482531185b87a00e82b1a936a3202f66
TJ Elder <telder55@gmail.com>
2021-01-29 12:52:11 -0700

    Got some javascript working for to compare current hour with hour block

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,49 @@ Homework Notes
 - Need to use local storage for saving in text. 
 - Need to use Moment.JS to get the times. 
 */
+
+var nineAM = document.getElementById("9");
+
+// Array for time block hours
+
+var hourBlockArray = [nineAM];
+
+function timeCheck() {
+    console.log("Howdy");
+    // Getting Hour as a single number
+    var currentHour = moment().toString().split(" ")[4].split(":")[0];
+    console.log(currentHour);
+
+    for (var i = 0; i < hourBlockArray.length; i++) {
+        // Getting hour in number for each block
+        var currentHourBlock = hourBlockArray[i].getAttribute("id");
+
+        // Console Logs to make sure these are numbers
+        console.log(currentHourBlock);
+        console.log(currentHour);
+        
+        // Compare times and add class 
+
+        if (currentHourBlock > currentHour) {
+            nineAM.classList.add("past");
+            
+        }
+    }
+
+    
+    
+
+};
+
+setInterval(timeCheck, 1000);
+
+
+
+
+
+// Current Date + Time
+let m = moment();
+
+
+// Logging Current Time
+console.log(`toString() ==> ${m.toString()}`);
