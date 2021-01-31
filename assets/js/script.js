22ec38ddb52b0b707fe2b7244e83faa13675fa13
TJ Elder <telder55@gmail.com>
2021-01-30 22:00:38 -0700

    Refactored 9am to scale to all time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,48 +28,77 @@ var hourBlockArray = [nineAM, tenAM, elevenAM, twelvePM, onePM, twoPM, threePM, 
 var textStoreArray = [];
 console.log(textStoreArray);
 
-// Get text from local storage 
-if (localStorage.getItem("Planner Entry")) {
-    textStoreArray = JSON.parse(localStorage.getItem("Planner Entry"))
-} else {
-    textStoreArray[0] = {
-        Nine: ""
-    };
-
+// Step 4 Get text from local storage 
+if (localStorage.getItem("Block Entry")) {
+    textStoreArray = JSON.parse(localStorage.getItem("Block Entry"))
+// } else {
+//     textStoreArray[0] = {
+//         Empty: ""
+//     };
 };
-
 console.log(textStoreArray);
+
+// Step 5 Loop through text store array and Add Block Text back to Same Block Text Area
+for (let i = 0; i < textStoreArray.length; i++) {
+    var hourNumber = textStoreArray[i].Block;
+    var hourContent = textStoreArray[i].BlockText;
+    console.log(hourNumber);
+    console.log(hourContent);
+    $("#" + hourNumber).val(hourContent)
+}
+    
+
 
 // Add 9am text to text area
 var nineText = document.getElementById("9-text");
 for (let i = 0; i < textStoreArray.length; i++) {
     nineText.textContent = textStoreArray[i].Nine;
-    
 }
 
 // Function to get 9am text 
-function getNineText(event) {
-    event.preventDefault();
-    var nineTextValue = document.getElementById("9-text").value;
-    console.log(nineTextValue)
-    store9Text(nineTextValue);
+// function getNineText(event) {
+//     event.preventDefault();
+//     var nineTextValue = document.getElementById("9-text").value;
+//     console.log(nineTextValue)
+//     store9Text(nineTextValue);
+// }
 
+// Step 2 Function to get text from text area of parent block that was clicked on. 
+function getBlockText() {
+    var closestBlockID = $(this).siblings('textarea').attr('id');
+    console.log(closestBlockID);
+    var closestBlockValue = $(this).siblings('textarea').val();
+    console.log(closestBlockValue);
+    storeBlockText(closestBlockID, closestBlockValue);
+}
+
+// Step 3 Store Block Text to Local Storage
+function storeBlockText(hour, text) {
+    var blockStore = {
+        Block: hour,
+        BlockText: text
+    }
+    textStoreArray.push(blockStore)
+    localStorage.setItem("Block Entry", JSON.stringify(textStoreArray))
 }
 
 // Store 9am text to local storage
-function store9Text(text) {
-    var nineStore = {
-        Nine: text
-    };
+// function store9Text(text) {
+//     var nineStore = {
+//         Nine: text
+//     };
     
-    // change to 
-    textStoreArray.push(nineStore);
-    // Stringify Text Store Array
-    localStorage.setItem("Planner Entry", JSON.stringify(textStoreArray));
-}
+//     // change to 
+//     textStoreArray.push(nineStore);
+//     // Stringify Text Store Array
+//     localStorage.setItem("Planner Entry", JSON.stringify(textStoreArray));
+// }
 
+// Event Listener for 9AM Save Button
+// saveNine.addEventListener("click", getNineText);
 
-saveNine.addEventListener("click", getNineText);
+// Step 1 Event Listener for any save button. Replacing previous event listener. 
+$(document).on("click", ".saveBtn", getBlockText);
 
 // Function to set colors based on current hour
 function timeCheck() {
